3eb2ce1b14dfc3090f802dac4f5e0392b5347000
lizcoding <elizarn96@gmail.com>
2021-12-06 11:31:18 -0800

    Modify wordcount.py

--- wordcount.py
+++ wordcount.py
@@ -1,4 +1,20 @@
 """Count words in file."""
+def wordcount(filename):
+    with open(filename) as file:
+        word_count = {}
+        data = file.read().split()
+        for letter in data:
+            word_count[letter] = word_count.get(letter, 0) + 1
+        for key,value in word_count.items():
+            print(f'{key}: {value}')
 
 
-# put your code here.
+wordcount('test.txt')
+
+def supercount(filename):
+    with open(filename) as file:
+        data = file.read().split()
+        for letter in set(data):
+            print(f'{letter}: {data.count(letter)}')
+
+supercount('test.txt')
